f0e5152353b45df9c7c6599379934bc20c221095
Luis Antonio <luis.developer.ac@gmail.com>
2022-10-04 19:06:49 -0500

    questao 1647

---
+++ 1647/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+#define debug(message, x) cout << message << ": " << x << endl
+#define printV(vector)    \
+    for (auto x : vector) \
+        cout << x << " "; \
+    cout << endl
+#define ulld unsigned long long int
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int num_bacias;
+    while (true)
+    {
+        cin >> num_bacias;
+
+        if (num_bacias == 0)
+            break;
+
+        vector<ulld> bacias(num_bacias);
+        for (int i = 0; i < num_bacias; i++)
+            cin >> bacias[i];
+
+        ulld soma_agregada = 0;
+
+        for (int i = bacias.size() - 1; i >= 0; i--)
+        {
+            bacias[i] += soma_agregada;
+            soma_agregada += bacias[i];
+        }
+
+        cout << soma_agregada << endl;
+    }
+
+    return 0;
+}
