a2a7efe83262efeccdd1d9544bdb2ef5226784a3
vrcvieira <victor.vieira@ifac.edu.br>
2022-10-06 18:54:54 -0500

    não lembro dessas alterações

---
+++ Treinamento interno - Leigo/3300/main.cpp
@@ -0,0 +1,18 @@
+// 3300
+
+// inverter um valor
+
+#include <stdio.h>
+
+int main ()
+{
+	char n[100000];
+	scanf("%s", &n);
+	printf("%s", n);
+	bool result = false;
+/* 	while (scanf("%d", &qtd) != EOF)
+	{
+		printf("\n");
+	}
+ */
+}
