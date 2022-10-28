47a60d3362eeb367ade6f4ff9ac818b8a10d482c
Luis Antonio <luis.developer.ac@gmail.com>
2022-10-27 19:33:12 -0500

    arruma as pastas do repositorio

---
+++ Notacao Polonesa/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    queue<char> fila;
+    stack<char> pilha;
+
+    string expressao;
+    cin >> expressao;
+
+    for (auto caracter : expressao)
+    {
+        if (caracter == '(')
+        {
+            pilha.push(caracter);
+        }
+
+        if (isalpha(caracter))
+        {
+            fila.push(caracter);
+        }
+
+        if (caracter == '+' || caracter == '-' || caracter == '/' || caracter == '*')
+        {
+            pilha.push(caracter);
+        }
+
+        if (caracter == ')')
+        {
+            while (fila.size() > 0)
+            {
+                cout << fila.front();
+                fila.pop();
+            }
+
+            cout << pilha.top();
+            pilha.pop();
+        }
+    }
+
+    while (pilha.size() > 0)
+    {
+        if (pilha.top() != '(')
+            cout << pilha.top();
+
+        pilha.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
--- regional_sbc/A/main.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input())
-n2 = int(input())
-
-print(n1 + n2)
