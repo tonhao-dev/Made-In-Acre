2beac2e22f0071ce2aea37e4b4071778bc713980
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-17 19:43:10 -0500

    2815 digitador gago

--- Template/main.cpp
+++ Template/main.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 #define db(message, x) cout << message << ": " << x << endl
-#define cv(vector)      \
-  for (auto x : vector) \
-    cout << x << " ";   \
-  cout << endl
+#define cv(vector)        \
+    for (auto x : vector) \
+        cout << x << " "; \
+    cout << endl
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
@@ -15,8 +15,8 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main(int argc, char **argv) {
-  cin.tie(0)->sync_with_stdio(0);
-  cin.exceptions(cin.failbit);
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
 
-  return 0;
+    return 0;
 }
