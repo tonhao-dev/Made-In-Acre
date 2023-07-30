c328092b6a7331a6933ecfe1479191f5517440da
Luis Santiago <luis.developer.ac@gmail.com>
2023-07-29 21:05:05 -0500

    conflito na do jogo do bixo

--- beecrowd/1483/main.cpp
+++ beecrowd/1483/main.cpp
@@ -1,26 +1,15 @@
 /**
-<<<<<<< HEAD
  * https://www.beecrowd.com.br/judge/pt/problems/view/1483
  * Ad hoc
-=======
- * [Nome da questão]
- * beecrowd | [Número]
- * [Link]
- * [Assuntos]
->>>>>>> main
  */
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-<<<<<<< HEAD
 #define SPEED cin.tie(0)->sync_with_stdio(0);
 #define db(x) cout << #x << ": " << x << endl
 #define db_pair(x) cout << #x << ": " << x.f << ", " << x.s << endl
-=======
-#define db(x) cout << #x << ": " << x << endl
->>>>>>> main
 #define cv(vector)        \
     for (auto x : vector) \
         cout << x << " "; \
@@ -44,62 +33,6 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pll;
 typedef vector<ll> vll;
-<<<<<<< HEAD
-typedef long double ld;
-
-ll pegaGrupoDoBicho(ll raw1) {
-    ll n1 = raw1 % 100;
-
-    if(n1 == 100 || n1 == 0) return 24;
-    if(n1 % 4 == 0) return n1 % 100 / 4 - 1;
-    return (n1 - (n1 % 4)) / 4;
-}
-
-int main(int argc, char **argv) {
-    SPEED;
-    ld v;
-    ll m, n;
-
-    cout << fixed;
-    cout.precision(2);
-    while(cin >> v >> n >> m) {
-        if(v == 0 && n == 0 && m ==0)
-            break;
-
-        string aposta = "0000" + to_string(m);
-        string sorteio = "0000" + to_string(n);
-
-        if(aposta.substr(aposta.size() - 4, 4) == sorteio.substr(sorteio.size() - 4, 4)) {
-            cout << v * 3000.0 << endl;
-            continue;
-        }
-
-        if(aposta.substr(aposta.size() - 3, 3) == sorteio.substr(sorteio.size() - 3, 3)) {
-            cout << v * 500.0 << endl;
-            continue;
-        }
-
-        if(aposta.substr(aposta.size() - 2, 2) == sorteio.substr(sorteio.size() - 2, 2)) {
-            cout << v * 50.0 << endl;
-            continue;
-        }
-
-        bool ganhou = false;
-
-        for(ll i = n; i<=n+4; ++i) {
-            if(pegaGrupoDoBicho(i) == pegaGrupoDoBicho(m)) {
-                cout << v * 16.0 << endl;
-                ganhou = true;
-                break;
-            }
-        }
-
-        if(!ganhou) cout << 0.00 << endl;
-    }
-
-    return 0;
-}
-=======
 
 int main(int argc, char **argv) {
     cin.tie(0)->sync_with_stdio(0);
@@ -150,4 +83,3 @@ int main(int argc, char **argv) {
     return 0;
 }
 
->>>>>>> main
