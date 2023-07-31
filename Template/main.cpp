02c8f1de30e86052b0f68b3d9eb457ef530a7481
Luis Santiago <luis.developer.ac@gmail.com>
2023-07-30 19:32:05 -0500

    adiciona o macro read ao template

--- Template/main.cpp
+++ Template/main.cpp
@@ -10,16 +10,16 @@ using namespace std;
 #define SPEED cin.tie(0)->sync_with_stdio(0);
 #define db(x) cout << #x << ": " << x << endl
 #define db_pair(x) cout << #x << ": " << x.f << ", " << x.s << endl
-#define cv(vector)        \
-    for (auto x : vector) \
-        cout << x << " "; \
+#define print_vector(vector) \
+    for (auto x : vector)    \
+        cout << x << " ";    \
     cout << endl
-#define cm(map)                             \
+#define print_map(map)                      \
     for (auto e : map)                      \
         cout << e.first << " " << e.second; \
     cout << endl
 #define FOR(i, a, b) for (ll i = a; i < (b); ++i)
-#define foreach(a, b) for (auto &(a) : (b))
+#define foreach(a, b) for (auto&(a) : (b))
 #define rep(i, n) FOR(i, 0, n)
 #define Rep(i, a, b) for (ll i = a; i >= (b); i--)
 #define all(x) begin(x), end(x)
@@ -29,14 +29,25 @@ using namespace std;
 #define endl "\n"
 #define f first
 #define s second
-#define MOD 1e9+7
+#define MOD 1e9 + 7
 #define log(x) cout << x << endl
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 typedef vector<ll> vll;
 
-int main(int argc, char **argv) {
+template <typename T>
+void read(T& arg) {
+    std::cin >> arg;
+}
+
+template <typename T, typename... Args>
+void read(T& first, Args&... args) {
+    std::cin >> first;
+    read(args...);
+}
+
+int main(int argc, char** argv) {
     SPEED;
 
     return 0;
