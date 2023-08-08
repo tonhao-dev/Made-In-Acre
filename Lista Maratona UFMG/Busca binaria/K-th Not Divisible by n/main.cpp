1312460cbece6d330a7d894c6c50eef75b05ff2e
Luis Santiago <luis.developer.ac@gmail.com>
2023-08-07 22:11:42 -0500

    update main

--- Lista Maratona UFMG/Busca binaria/K-th Not Divisible by n/main.cpp
+++ Lista Maratona UFMG/Busca binaria/K-th Not Divisible by n/main.cpp
@@ -1,6 +1,6 @@
 /**
- * https://codeforces.com/contest/1352/problem/C
- * binary search, busca binaria, math, matematica
+ * [Link]
+ * [Assuntos]
  */
 
 #include <bits/stdc++.h>
@@ -10,18 +10,16 @@ using namespace std;
 #define SPEED cin.tie(0)->sync_with_stdio(0);
 #define db(x) cout << #x << ": " << x << endl
 #define db_pair(x) cout << #x << ": " << x.f << ", " << x.s << endl
-#define printv(vector)                             \
-    {                                              \
-        cout << #vector << " = " << ;              \
-        for (auto &it : vector) cout << it << " "; \
-        cout << endl;                              \
-    }
-#define cm(map)                             \
+#define print_vector(vector) \
+    for (auto x : vector)    \
+        cout << x << " ";    \
+    cout << endl
+#define print_map(map)                      \
     for (auto e : map)                      \
         cout << e.first << " " << e.second; \
     cout << endl
 #define FOR(i, a, b) for (ll i = a; i < (b); ++i)
-#define foreach(a, b) for (auto &(a) : (b))
+#define foreach(a, b) for (auto&(a) : (b))
 #define rep(i, n) FOR(i, 0, n)
 #define Rep(i, a, b) for (ll i = a; i >= (b); i--)
 #define all(x) begin(x), end(x)
@@ -33,51 +31,53 @@ using namespace std;
 #define s second
 #define MOD 1e9 + 7
 #define log(x) cout << x << endl
-#define read(x) cin >> x
-
-#define MAX_N 1e10
 
 typedef long long ll;
 typedef pair<ll, ll> pll;
 typedef vector<ll> vll;
 
-ll binary_search(ll inicio, ll fim, ll n, ll k) {
-    ll meio;
+template <typename T>
+void read(T& arg) {
+    std::cin >> arg;
+}
 
-    ll resp = -1;
-    while (inicio < fim) {
-        meio = (inicio + fim) / 2;
+template <typename T, typename... Args>
+void read(T& first, Args&... args) {
+    std::cin >> first;
+    read(args...);
+}
 
-        if (meio % n == 0) {
-            meio--;
-        }
+ll test_cases, n, k;
 
-        ll kth_position = meio - (meio / n);
+bool checaSeEstaProximoDoKth(ll x){
+    ll divisiveisPorX = x/n;
+    if((x - divisiveisPorX) >= k) return true;
+    return false;
+}
 
-        if (kth_position == k) {
-            resp = meio;
-            break;
-        };
+int buscaBinaria(){
+    ll esquerda = 1, direita = 10e9;
 
-        if (kth_position > k) {
-            fim = meio;
+    while(esquerda < direita) {
+        ll meio = (esquerda + direita) / 2;
+
+        if(checaSeEstaProximoDoKth(meio)){
+            direita = meio;
         } else {
-            inicio = meio + 1;
+            esquerda = meio + 1;
         }
     }
 
-    return resp;
+    return esquerda;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     SPEED;
-    ll t;
-    read(t);
-    rep(ti, t) {
-        ll n, k;
-        cin >> n >> k;
+    read(test_cases);
 
-        log(binary_search(1, MAX_N, n, k));
+    rep(test_case, test_cases) {
+        read(n, k);
+        log(buscaBinaria());
     }
 
     return 0;
