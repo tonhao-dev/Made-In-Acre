cd7b17b23e9087a566ed74aeb918959a7e328341
Tonhao <35160032+tonhao-dev@users.noreply.github.com>
2023-08-19 09:28:44 -0500

    Tonhao/pd (#14)
    * adiciona o assunto da questao 3351
    
    * frog jump
    
    * cortando canos

--- beecrowd/3351/main.cpp
+++ beecrowd/3351/main.cpp
@@ -1,5 +1,4 @@
-// https://www.beecrowd.com.br/judge/pt/problems/view/3351
-// busca binária
+// Busca binaria, binary search
 #include <bits/stdc++.h>
 
 #define speed                     \
@@ -36,11 +35,9 @@ typedef vector<vll> vvll;
 ull N, K;
 vector<pair<ull, ull>> pessoas(1e5 + 1);
 
-bool consegue(ull tempo)
-{
+bool consegue(ull tempo) {
     ull total_views = 0;
-    for (ull i = 0; i < N; i++)
-    {
+    for (ull i = 0; i < N; i++) {
         if (pessoas[i].f > tempo)
             continue;
         total_views++;
@@ -52,8 +49,7 @@ bool consegue(ull tempo)
     return total_views >= K;
 }
 
-int main()
-{
+int main() {
     speed;
     cin >> N >> K;
 
@@ -61,17 +57,13 @@ int main()
         cin >> pessoas[i].f >> pessoas[i].s;
 
     ull l = 0, r = 2 * 1e18, ans;
-    while (l < r)
-    {
+    while (l < r) {
         ull mid = (l + r) / 2;
 
-        if (consegue(mid))
-        {
+        if (consegue(mid)) {
             ans = mid;
             r = mid;
-        }
-        else
-        {
+        } else {
             l = mid + 1;
         }
     }
