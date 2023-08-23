5ab2b55579857b44d3458a4046c652c211ef10e1
igor maia <igormaia2001@gmail.com>
2023-08-22 21:50:04 -0500

    squares

---
+++ LPC I 2021/PD/E - Squares/main.cpp
@@ -0,0 +1,77 @@
+/**
+ * [Link]
+ * [Assuntos]
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SPEED cin.tie(0)->sync_with_stdio(0);
+#define db(x) cout << #x << ": " << x << endl
+#define db_pair(x) cout << #x << ": " << x.f << ", " << x.s << endl
+#define printv(vector)                             \
+    {                                              \
+        cout << #vector << " = ";                  \
+        for (auto& it : vector) cout << it << " "; \
+        cout << endl;                              \
+    }
+#define print_map(map)                      \
+    for (auto e : map)                      \
+        cout << e.first << " " << e.second; \
+    cout << endl
+#define FOR(i, a, b) for (ll i = a; i < (b); ++i)
+#define foreach(a, b) for (auto&(a) : (b))
+#define rep(i, n) FOR(i, 0, n)
+#define Rep(i, a, b) for (ll i = a; i >= (b); i--)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define f first
+#define s second
+#define MOD 1e9 + 7
+#define log(x) cout << x << endl
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+
+template <typename T>
+
+void read(T& arg) {
+    std::cin >> arg;
+}
+
+template <typename T, typename... Args>
+void read(T& first, Args&... args) {
+    std::cin >> first;
+    read(args...);
+}
+
+ll memo[10001];
+
+void dp() {
+    memo[0] = 0;
+    for(ll i = 1; i<=10000; i++) {
+        memo[i] = i;
+        for(ll j=1; j*j <= i; j++) {
+            memo[i] = min(memo[i], memo[i-j*j] + 1);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    SPEED;
+    dp();
+    ll test_cases, number;
+    read(test_cases);
+
+    rep(test_case, test_cases) {
+        read(number);
+        log(memo[number]);
+    }
+
+    return 0;
+}
