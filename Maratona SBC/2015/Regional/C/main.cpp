6869aec69276d289c2c42415de9e94583322cbc7
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2024-03-18 13:34:17 -0500

    adicionando questoes regional de 2015

---
+++ Maratona SBC/2015/Regional/C/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problema: C - Tri-du
+ * Assunto: Iniciante
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main (int arg, char** argv) {
+    int a, b;
+
+    cin >> a >> b;
+
+    if(a >= b) {
+        cout << a << endl;
+    } else {
+        cout << b << endl;
+    }
+
+    return 0;
+}
