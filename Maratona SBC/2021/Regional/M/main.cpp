da7fb1b6530692188f19242425af991066a274cd
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2024-03-24 01:21:02 -0500

    adicionando questao N na regional de 2023

--- Maratona SBC/2021/Regional/M/main.cpp
+++ Maratona SBC/2021/Regional/M/main.cpp
@@ -8,102 +8,45 @@
 
 using namespace std;
 
-#define SPEED cin.tie(0)->sync_with_stdio(0);
-#define db(x) cout << #x << ": " << x << endl
-#define db_pair(x) cout << #x << ": " << x.f << ", " << x.s << endl
-#define cv(vector)        \
-    for (auto x : vector) \
-        cout << x << " "; \
-    cout << endl
-#define cm(map)                             \
-    for (auto e : map)                      \
-        cout << e.first << " " << e.second; \
-    cout << endl
-#define FOR(i, a, b) for (ll i = a; i < (b); ++i)
-#define foreach(a, b) for (auto &(a) : (b))
-#define rep(i, n) FOR(i, 0, n)
-#define Rep(i, a, b) for (ll i = a; i >= (b); i--)
-#define all(x) begin(x), end(x)
-#define sz(x) (ll)(x).size()
+#define endl '\n'
 #define pb push_back
-#define pf push_front
-#define endl "\n"
-#define f first
-#define s second
+#define pi pair<int, int>
+#define pii pair<int, pi>
+#define fir first
+#define sec second
+#define MAXN 300005
 
-typedef long long ll;
-typedef pair<ll, ll> pll;
-typedef vector<ll> vll;
+int id = 1;
+vector<int> adj[MAXN];
+bool vis[MAXN];
+queue<int> qq;
 
-class Node {
-   public:
-    vector<ll> vizinhos;
-    bool isLive = true;
-};
-
-class Graph {
-   public:
-    vector<Node> children;
-    ll num_nodes;
-    bool found = false;
-
-    Graph(ll num_nodes) {
-        this->children.resize(num_nodes + 10);
-        this->num_nodes = num_nodes;
-    }
-
-    void add_edge(ll from, ll to) {
-        this->children[from].vizinhos.push_back(to);
-    }
-
-    void find_people_to_kill(ll find_node, void (*mark_as_visited)(Node *)) {
-        mark_as_visited(&this->children[find_node]);
-    }
-
-    void find_first_people_live(ll current_node) {
-        if(this->found) return;
-
-        if(this->children[current_node].isLive) {
-            cout << current_node << endl;
-            this->found = true;
-            return;
-        };
-
-        for (auto next_node_index : this->children[current_node].vizinhos) {
-            find_first_people_live(next_node_index);
-        }
-    }
-};
-
-bool is_people_live(Node node) {
-    return node.isLive;
+void dfs(int s) {
+    qq.push(s);
+    for (auto const &i : adj[s])
+        dfs(i);
 }
-
-void set_people_die(Node *node) {
-    node->isLive = false;
-}
-
-int main(int argc, char **argv) {
-    SPEED;
-
-    ll vertices;
-    cin >> vertices;
-
-    Graph grafo(vertices);
-
-    ll currentId = 2;
-    rep(i, vertices) {
-        ll event, id;
-        cin >> event >> id;
-
-        if(event == 1) {
-            grafo.add_edge(id, currentId++);
-        } else {
-            grafo.find_people_to_kill(id, set_people_die);
-            grafo.found = false;
-            grafo.find_first_people_live(1);
-        }
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int q;
+    cin >> q;
+    vector<int> queries;
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        b--;
+        if (a == 1)
+            adj[b].pb({id++});
+        else
+            queries.pb(b);
     }
-
+    dfs(0);
+    for (auto const &i : queries) {
+        vis[i] = 1;
+        while (vis[qq.front()])
+            qq.pop();
+        cout << qq.front() + 1 << endl;
+    }
     return 0;
 }
