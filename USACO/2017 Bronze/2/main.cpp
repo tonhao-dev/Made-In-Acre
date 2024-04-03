c4f202476db2886c7e3fc67917eebab2dde2c227
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2024-04-03 10:23:08 -0500

    criando problemas da USACO

---
+++ USACO/2017 Bronze/2/main.cpp
@@ -0,0 +1,87 @@
+/**
+ * USACO 2017 December Contest, Bronze
+ * Problem 2. The Bovine Shuffle
+ * Adhoc, Simulação
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SPEED cin.tie(0)->sync_with_stdio(0);
+#define DEBUG true
+#define db(x) \
+    if (DEBUG) cout << #x << ": " << x << endl
+#define dbpair(x) \
+    if (DEBUG) cout << #x << ": " << x.f << ", " << x.s << endl
+#define dbvector(vector)                           \
+    {                                              \
+        cout << #vector << " = ";                  \
+        for (auto& it : vector) cout << it << " "; \
+        cout << endl;                              \
+    }
+#define dbmap(map)                          \
+    for (auto e : map)                      \
+        cout << e.first << " " << e.second; \
+    cout << endl
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define f first
+#define s second
+#define MOD 1e9 + 7
+#define mp make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+// Input/Output - s is basically the file name without the ".in" and ".out"
+
+void setIO(string s) {
+	freopen((s+".in").c_str(),"r",stdin);
+	freopen((s+".out").c_str(),"w",stdout);
+}
+
+
+int main(int argc, char** argv) {
+    SPEED;
+
+    setIO("shuffle");
+
+    int n; cin >> n;
+    vector<int> posi(n + 1);
+    vector<string> vacas(n + 1);
+
+    for(int i = 1; i <= n; i++) {
+        cin >> posi[i];
+    }
+    for(int i = 1; i <= n; i++) {
+        cin >> vacas[i];
+    }
+
+    int embaralhamentos = 3;
+    while(embaralhamentos--) {
+        vector<string> temp(n + 1);
+
+        for(int i = 1; i <= n; i++) {
+            auto ant_posi = find(all(posi), i) - posi.begin();
+            temp[ant_posi] = vacas[i];
+        }
+
+        vacas = temp;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        cout << vacas[i] << endl;
+    }
+
+    return 0;
+}
