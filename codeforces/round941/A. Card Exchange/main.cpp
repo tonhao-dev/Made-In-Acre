da7781d032d2146feabaf1b6f3c1a7fb1ca47587
igor maia <igormaia2001@gmail.com>
2024-04-27 10:36:49 -0500

    codeforces 941

---
+++ codeforces/round941/A. Card Exchange/main.cpp
@@ -0,0 +1,90 @@
+/**
+ * [Link]
+ * [Título da questão]
+ * [Assuntos]
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SPEED cin.tie(0)->sync_with_stdio(0);
+#define DEBUG false
+#define db(x) \
+    if (DEBUG) cout << #x << ": " << x << endl
+#define dbpair(x) \
+    if (DEBUG) cout << #x << ": " << x.f << ", " << x.s << endl
+#define dbvector(vector)                           \
+    {                                              \
+        cout << #vector << " = ";                  \
+        for (auto& it : vector) cout << it << " "; \
+        cout << endl;                              \
+    }
+#define dbmap(map)                          \
+    for (auto e : map)                      \
+        cout << e.first << " " << e.second; \
+    cout << endl
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define pf push_front
+#define endl "\n"
+#define f first
+#define s second
+#define MOD 1e9 + 7
+#define mp make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve() {
+    int n, k, number, ans;
+    cin >> n >> k;
+    ans = n;
+    map<int, int> numbers;
+    for(int i = 0; i<n; i++) {
+        cin >> number;
+        numbers[number]++;
+    }
+    while(true) {
+        int max = INT_MIN/2;
+        int key = 0;
+        for(auto e: numbers) {
+            if(e.second > max && e.second >= k) {
+                max = e.second;
+                key = e.first;
+            }
+        }
+        if(key == 0) break;
+        db(key);
+        numbers[key]-=k;
+        max = INT_MIN/2;
+        for(auto e: numbers) {
+            if(e.second > max) {
+                max = e.second;
+                key = e.first;
+            }
+        }
+        db(key);
+        ans--;
+        numbers[key]+=(k-1);
+    }
+    cout << ans << endl;
+}
+
+int main(int argc, char** argv) {
+    SPEED;
+
+    int t;
+    cin >> t;
+
+    while (t--) solve();
+
+    return 0;
+}
